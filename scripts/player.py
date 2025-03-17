85360114bcd726767c30594699ff2772c73b269a
cbgpenguin <cbaneygiampoala@gmail.com>
2025-03-16 23:56:09 -0400

    Yay + trying to do things and failing

--- scripts/player.py
+++ scripts/player.py
@@ -6,26 +6,45 @@ from py4godot.classes.core import Vector3
 from py4godot.classes.CharacterBody2D.CharacterBody2D import CharacterBody2D
 
 @gdclass
-class player(CharacterBody2D):
+class Player(CharacterBody2D):
 
 	# define properties like this
-	test_int = 5
-	test_float= 5.2
-	test_bool = True
-	test_vector = Vector3.new3(1,2,3)
+	
+	SPEED = 130.0
+	JUMP_VELOCITY = -300.0
+	
+	# Get the gravity from the project settings to be synced with RigidBody nodes.
+	gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 	# define signals like this
-	test_signal = signal([SignalArg("test_arg", int)])
+	#test_signal = signal([SignalArg("test_arg", int)])
 
 
 	def _ready(self) -> None:
-		pass
-		# put initialization code here
+		animatedSprite = $AnimatedSprite2D
 
 	def _process(self, delta:float) -> None:
-		pass
-		# put dynamic code here
+		direction = Input.get_axis("move_left", "move_right")
+		#flip sprite
+		if direction > 0:
+			animatedSprite.flip_h = false
+		elif direction < 0:
+			animatedSprite.flip_h = true
+		#Play animation
+		if direction == 0:
+			animatedSprite.play("Idle")
+		else:
+			animatedSprite.play("run")
+		#apply momvement
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 
+		
+	
 	# Hide the method in the godot editor
 	@private
 	def test_method(self):
