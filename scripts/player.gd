affeb3f8be458fbe591a76c281a8f57984e28996
cbgpenguin <cbaneygiampoala@gmail.com>
2025-04-30 12:23:07 -0400

    animation stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,25 +2,33 @@ extends CharacterBody2D
 
 const SPEED = 250.0 * 60 # Pixles/s
 @onready var animatedSprite2d: AnimatedSprite2D = $AnimatedSprite2D
+var colidedLastFrame = true
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	# Direction is -1, 1, or 0
 	if direction:
 		velocity.x = direction * SPEED * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, (1000 * delta)) #Slows it down
 	# Animation
-	if direction == 0:
-		animatedSprite2d.flip_h = false
-		animatedSprite2d.play("default (front face short idel)")
-	elif direction == 1:
-		animatedSprite2d.flip_h = false
-		animatedSprite2d.play("walking (self explanatory but I felt like giving a description)")
-	elif direction == -1:
-		animatedSprite2d.flip_h = true
-		animatedSprite2d.play("walking (self explanatory but I felt like giving a description)")
-		
-	move_and_slide()
+	if !colidedLastFrame:
+		if direction == 0:
+			setDefaltAnimation()
+		elif direction == 1:
+			animatedSprite2d.flip_h = false
+			animatedSprite2d.play("walking (self explanatory but I felt like giving a description)")
+		elif direction == -1:
+			animatedSprite2d.flip_h = true
+			animatedSprite2d.play("walking (self explanatory but I felt like giving a description)")
+	else:
+		setDefaltAnimation()
+	
+	
+	colidedLastFrame = move_and_slide()
+
+func setDefaltAnimation():
+	animatedSprite2d.flip_h = false
+	animatedSprite2d.play("default (front face short idel)")
